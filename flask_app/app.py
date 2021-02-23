15b9fb4f023523ec32a21103694b364644be4cd6
Melody Peterson <melodygr@aol.com>
2021-02-23 16:51:50 -0500

    add post

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
     return 'This is my first Flask app! Yay!'
+
+@app.route('/bob')
+def bobpage():
+    return 'Yo BOB!'
+
+@app.route('/method', methods=['GET', 'POST'])
+def method():
+    if request.method == 'POST':
+        return "You've used a POST request!"
+    else:
+        return "I reckon using a GET request!"    
 
 
 app.run()
