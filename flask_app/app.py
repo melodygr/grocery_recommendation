1dde591db8183273861001ef3721c9f5c225a044
Melody Peterson <melodygr@aol.com>
2021-02-23 17:52:06 -0500

    added form

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,19 +3,11 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def welcome():
-    return render_template('index.html')
-
-@app.route('/bob')
-def bobpage():
-    return 'Yo BOB!'
-
-@app.route('/method', methods=['GET', 'POST'])
-def method():
-    if request.method == 'POST':
-        return "You've used a POST request!"
-    else:
-        return "I reckon using a GET request!"    
-
+def rootpage():
+    name = ''
+    if request.method == 'POST' and 'username' in request.form:
+        name = request.form.get('username')
+    return render_template('index.html',
+                            name=name)
 
 app.run()
