232fdff65e500fe54c7023917415343c34173353
Melody Peterson <melodygr@aol.com>
2021-02-23 22:00:58 -0500

    debug

--- flask_app/app.py
+++ flask_app/app.py
@@ -15,7 +15,7 @@ def bmipage():
     bmi = ''
     print('before the if statement')
     print(request.method)
-    print(request.form)
+    print('userheight' in request.form)
     if request.method == 'POST' and 'userheight' in request.form:
         print('I am in the if statement')
         height = float(request.form.get('userheight'))
