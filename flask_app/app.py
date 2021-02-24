659d0268859b01a823e1f112b5602b51bd8aa1a3
Melody Peterson <melodygr@aol.com>
2021-02-24 18:42:42 -0500

    fixed string

--- flask_app/app.py
+++ flask_app/app.py
@@ -45,7 +45,7 @@ def nlppage():
             item_count = len(nlp)
             nlp0_id = nlp.index[0]
             nlp0_name = nlp.iloc[0]    
-    return render_template(nlp.html,
+    return render_template('nlp.html',
                            nlp=nlp, 
                            nlp0_id=nlp0_id, 
                            nlp0_name=nlp0_name)    
