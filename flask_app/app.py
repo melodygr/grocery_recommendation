c019ebd7ea771970a138decad938d62a7a6ed0ff
Melody Peterson <melodygr@aol.com>
2021-02-23 21:15:07 -0500

    corrected

--- flask_app/app.py
+++ flask_app/app.py
@@ -14,12 +14,15 @@ def rootpage():
 def bmipage():
     height = 0
     weight = 0
-    bmi = 0
+    bmi = ''
     if request.method == 'POST' and 'userheight' in request.form:
-        height = request.form.get('userheight')
-        weight = request.form.get('userweight')
-        bmi = (weight/height/height)*703
+        height = float(request.form.get('userheight'))
+        weight = float(request.form.get('userweight'))
+        bmi = calc_bmi(weight, height)
     return render_template('bmi.html',
                             bmi=bmi)
+
+def calc_bmi(weight, height):
+    return round((weight/height/height)*703, 2)                            
 
 app.run()
