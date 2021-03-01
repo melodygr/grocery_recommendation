422c0e2284e71ce13f203fe6d283ed1a3b7de57e
Melody Peterson <melodygr@aol.com>
2021-03-01 00:27:11 -0500

    debug

--- flask_app/app.py
+++ flask_app/app.py
@@ -38,7 +38,7 @@ def nlppage():
     nlp = ''
     if request.method == 'POST' and 'searchwords' in request.form:
         nlp = stem_and_vectorize_products_based_on_metadata(request.form.get('searchwords'))
-        if nlp == None:
+        if not nlp.any():
             nlp=0
     return render_template('nlp.html',
                            nlp=nlp, 
